dba3bc2d781ca17c2a827b1a1b6a9902316b0bea
Nadiia Maltseva <amanavi2021@gmail.com>
2023-05-07 23:51:52 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Searchbar } from "components/Searchbar/Searchbar";
+
+export class App extends Component {
+
+  state = {
+    pictureName: '',
+  };
+
+   handleSearchFormSubmit = pictureName=> {
+    this.setState({ pictureName });
+   };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSearchFormSubmit}/>
+      </div>
+    );
+  };
+  
 };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,44 @@
+import { Component } from "react";
+import PropTypes from 'prop-types';
+import { SearchBar, SearchForm, SearchFormButton, SearchFormLabel, SearchFormInput} from "./Searchbar.styled";
+
+export class Searchbar extends Component {
+    state = {
+        pictureName:'',
+    };
+
+    handleNameChange = event => {
+        this.setState({pictureName: event.currentTarget.value.toLowerCase()})
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if(this.state.pictureName.trim() === ''){
+            return alert('Ввeдите название картинки')
+        };
+
+        this.props.onSubmit(this.state.pictureName);
+        this.setState({pictureName: ''})
+    };
+   
+    render() {
+        return (
+            <SearchBar>
+            <SearchForm onSubmit={this.handleSubmit}>
+              <SearchFormButton type="submit">
+                <SearchFormLabel>Search</SearchFormLabel>
+              </SearchFormButton>          
+              <SearchFormInput
+                type="text"
+                autocomplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+                value={this.state.pictureName}
+                onChange={this.handleNameChange}
+              />
+            </SearchForm>
+          </SearchBar> 
+        )
+    }
+
+}
